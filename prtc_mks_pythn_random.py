616172c473211e5ae8626c860ace86e40c9c5d24
bitsreset <bitsreset@gmail.com>
2017-02-14 16:10:19 -0800

    Update prtc_mks_pythn_random.py

--- prtc_mks_pythn_random.py
+++ prtc_mks_pythn_random.py
@@ -1,3 +1,7 @@
+#Write a program that chooses a random integer between 0 and 100 (inclusive).
+#Then ask the user to guess what number has been chosen. Each time the user enters a guess, 
+#the program indicates whether the user guessed correctly (and exits), or if the guess was too high or too low.
+
 import random
 answer = random.randint(0,100)
 guess = int(input("Guess a no. [0,100]"))
