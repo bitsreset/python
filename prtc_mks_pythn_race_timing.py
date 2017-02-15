a959610ba44a85a165dae0f49daf4d6cd2eb3cc3
bitsreset <bitsreset@gmail.com>
2017-02-14 16:19:04 -0800

    Race timing

---
+++ prtc_mks_pythn_race_timing.py
@@ -0,0 +1,14 @@
+#Each day, you run 10 km, and want to know how long, on average, that run
+#takes. Write a program that asks the user how long it took to run 10 km today.
+#The programs continues to ask how long (in minutes) it took, until the user
+#presses Enter. At that point, the program exits—but only after calculating
+#and displaying the average time that the 10 km run took.
+#
+#For example, here is what the program would look like if the user enters
+#three data points:
+#
+#Enter 10 km run time: 15
+#Enter 10 km run time: 20
+#Enter 10 km run time: 10
+#Enter 10 km run time:
+#Average of 15.0, over 3 races
