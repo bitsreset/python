d6d602b3aa75577c1c17649134ed2259d8cabaef
bitsreset <bitsreset@gmail.com>
2017-05-27 13:28:45 -0700

    find_ascii_code.py
    (Find the character of an ASCII code) Write a program that receives an ASCII
    code (an integer between 0 and 127) and displays its character. For example, if the
    user enters 97, the program displays the character a.

---
+++ find_ascii_code.py
@@ -0,0 +1,9 @@
+Created on Sat May 27 13:17:57 2017
+
+@author: joykumardas
+(Find the character of an ASCII code) Write a program that receives an ASCII
+code (an integer between 0 and 127) and displays its character. For example, if the
+user enters 97, the program displays the character a.
+"""
+ascii_code = int(input("Enter and ASCII Code:"))
+print("The character is:",chr(ascii_code))
